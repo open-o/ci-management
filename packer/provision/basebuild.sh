1cc3afd4affd9034ca81b2c37164226d5904df4d
Gary Wu <gary.i.wu@huawei.com>
2016-09-09 13:41:22 -0700

    Add python dependencies to basebuild images
    Change-Id: If64a8b6e6cf9b8b570450ef28f5471a646e60869
    Issue-id: INT-7
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

--- packer/provision/basebuild.sh
+++ packer/provision/basebuild.sh
@@ -1,4 +1,37 @@
-#!/bin/bash -x
-# vim: set tw=4 sw=4 sts=4 et :
+#!/bin/bash
 
-# Presently nothing to do
+# vim: ts=4 sw=4 sts=4 et tw=72 :
+
+rh_systems() {
+    # Install python dependencies
+    yum install -y python-{devel,virtualenv,setuptools,pip}
+}
+
+ubuntu_systems() {
+    # Install python dependencies
+    apt install -y python-{dev,virtualenv,setuptools,pip}
+}
+
+all_systems() {
+    echo 'No common distribution configuration to perform'
+}
+
+echo "---> Detecting OS"
+ORIGIN=$(facter operatingsystem | tr A-Z a-z)
+
+case "${ORIGIN}" in
+    fedora|centos|redhat)
+        echo "---> RH type system detected"
+        rh_systems
+    ;;
+    ubuntu)
+        echo "---> Ubuntu system detected"
+        ubuntu_systems
+    ;;
+    *)
+        echo "---> Unknown operating system"
+    ;;
+esac
+
+# execute steps for all systems
+all_systems

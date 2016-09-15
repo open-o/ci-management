c197ae19fd6926f1a3a2edcd548b87ecc9ca18b7
Gary Wu <gary.i.wu@huawei.com>
2016-09-15 14:07:03 -0700

    Add basic autorelease job to do integrated clean build
    Change-Id: I17e4de2aa37f3cd7b4e70358db8abd1cc6a3f3fb
    Issue-id: INT-21
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

---
+++ jjb/integration/include-raw-clone-all-projects.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This should be run from integration repo
+
+ROOT=`git rev-parse --show-toplevel`/autorelease
+
+BUILD_DIR=$ROOT/build
+
+mkdir -p $BUILD_DIR
+cd $BUILD_DIR
+
+while read p; do
+    rm -r $BUILD_DIR/$p
+    #TODO: replace with https once repo is open to public
+    git clone ssh://openo-jobbuilder@gerrit.open-o.org:29418/$p
+done < $ROOT/all-projects.txt
+

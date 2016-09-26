0ea43f51de965aff2cdcdfeebf8e56467472cca9
Gary Wu <gary.i.wu@huawei.com>
2016-09-26 10:52:30 -0700

    Refactor CSIT job definitions
    Change-Id: I74275e47b6077536a1d117d2e894d819418050a6
    Issue-id: INT-6
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

--- jjb/integration/include-raw-integration-run-test.sh
+++
@@ -1,7 +0,0 @@
-#@IgnoreInspection BashAddShebang
-# Activate robotframework virtualenv
-# ${ROBOT_VENV} comes from the include-raw-integration-install-robotframework.sh
-# script.
-
-${WORKSPACE}/test/csit/run-csit.sh ${TESTPLAN} ${TESTOPTIONS}
-

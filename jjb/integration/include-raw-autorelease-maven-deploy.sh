6ef6c83405790bfdbc398ae2a6cc34295c993e8b
Andrew Grimberg <agrimberg@linuxfoundation.org>
2016-09-28 12:34:05 -0700

    Configure autorelease to push to staging repository
    Setup the autorelease project to push artifacts to an autocreated
    staging repository.
    
    Change-Id: I5f5945992de1037a99bda0ec6f549d3171c52da9
    Signed-off-by: Andrew Grimberg <agrimberg@linuxfoundation.org>

---
+++ jjb/integration/include-raw-autorelease-maven-deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# @License EPL-1.0 <http://spdx.org/licenses/EPL-1.0>
+##############################################################################
+# Copyright (c) 2015 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+
+# Assuming that mvn deploy created the hide/from/pom/files/stage directory.
+cd hide/from/pom/files
+mkdir -p m2repo/org/openo/
+
+(IFS='
+'
+for m in `xmlstarlet sel -N x=http://maven.apache.org/POM/4.0.0 -t -m '//x:modules' -v '//x:module' ../../../../pom.xml`; do
+    rsync -avz --exclude 'maven-metadata*' \
+               --exclude '_remote.repositories' \
+               --exclude 'resolver-status.properties' \
+               "stage/org/openo/$m" m2repo/org/openo/
+done)
+
+mvn -V -B org.sonatype.plugins:nexus-staging-maven-plugin:1.6.2:deploy-staged-repository \
+    -DrepositoryDirectory="`pwd`/m2repo" \
+    -DnexusUrl=https://nexus.open-o.org/ \
+    -DstagingProfileId="d8330dc636933" \
+    -DserverId="openo.staging" \
+    -s $SETTINGS_FILE \
+    -gs $GLOBAL_SETTINGS_FILE | tee $WORKSPACE/deploy-staged-repository.log

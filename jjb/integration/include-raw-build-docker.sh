d1d9fc87e0cf0c0b7fc4a6eb99d8f5a137aa7de3
Gary Wu <gary.i.wu@huawei.com>
2017-02-10 13:51:15 -0800

    Use docker-maven-plugin to do docker push
    Change-Id: I76941802b8642f248d36cee9d1bbeb90127b6037
    Issue-id: INT-80
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

--- jjb/integration/include-raw-build-docker.sh
+++ jjb/integration/include-raw-build-docker.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 $WORKSPACE/test/csit/docker/scripts/gen-all-dockerfiles.sh
 $WORKSPACE/test/csit/docker/scripts/build-all-images.sh
-$WORKSPACE/test/csit/docker/scripts/push-all-images.sh
